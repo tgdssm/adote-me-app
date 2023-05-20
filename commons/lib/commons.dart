de03006596149b921b0d89d645048b663632b276
tgdssm <thalissongabriel1999@gmail.com>
2023-05-19 21:50:54 -0300

    feat: app http client service

--- commons/lib/commons.dart
+++ commons/lib/commons.dart
@@ -1,1 +1,4 @@
 library commons;
+
+export 'services/app_http_client/app_http_client_service.dart';
+export 'services/app_http_client/app_http_client_impl.dart';
---
+++ commons/lib/services/app_http_client/app_http_client_impl.dart
@@ -0,0 +1,72 @@
+import 'package:dependencies/dependencies.dart';
+
+import 'app_http_client_service.dart';
+
+class AppHttpClientDioServiceImpl implements IAppHttpClientService {
+  final Dio _dio;
+
+  AppHttpClientDioServiceImpl(this._dio);
+
+  @override
+  Future delete(
+    String url, {
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? queryParameters,
+    Map<String, String>? headers,
+  }) {
+    return _dio.delete(
+      url,
+      data: body,
+      queryParameters: queryParameters,
+      options: Options(headers: headers),
+    );
+  }
+
+  @override
+  Future get(
+    String url, {
+    Map<String, dynamic>? queryParameters,
+    Map<String, String>? headers,
+  }) {
+    return _dio.get(
+      url,
+      queryParameters: queryParameters,
+      options: Options(headers: headers),
+    );
+  }
+
+  @override
+  Future post(
+    String url,
+    Map<String, dynamic> body, {
+    Map<String, dynamic>? queryParameters,
+    Map<String, String>? headers,
+    String? contentType,
+  }) {
+    return _dio.post(
+      url,
+      data: body,
+      queryParameters: queryParameters,
+      options: Options(headers: headers, contentType: contentType),
+    );
+  }
+
+  @override
+  Future put(
+    String url,
+    Map<String, dynamic> body, {
+    Map<String, dynamic>? queryParameters,
+    Map<String, String>? headers,
+    String? contentType,
+  }) {
+    return _dio.put(
+      url,
+      data: body,
+      queryParameters: queryParameters,
+      options: Options(
+        headers: headers,
+        contentType: contentType,
+      ),
+    );
+  }
+}
